f5f71a1926d01c035ad98b01b716fc1774e21f3d
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2021-03-03 19:28:59 +0100

    check-operation-status-worker

--- src/processes/workers/check-operation-status/index.ts
+++ src/processes/workers/check-operation-status/index.ts
@@ -3,6 +3,23 @@ import * as cron from 'cron';
 import { createLogger } from '../../../services/logger';
 import { cronTime, loggerConfig } from './config';
 import { CheckOperationStatusProcess } from './check-operation-status-process';
+import {PrometheusExporter} from "@opentelemetry/exporter-prometheus";
+import {MeterProvider} from "@opentelemetry/metrics";
+
+// Add your port and startServer to the Prometheus options
+const options = {port: 9464, startServer: true};
+const exporter = new PrometheusExporter(options);
+
+// Register the exporter
+const meter = new MeterProvider({
+  exporter,
+  interval: 1000,
+}).getMeter('example-prometheus');
+
+const counter = meter.createCounter('metric_name', {
+  description: 'Example of a counter'
+});
+counter.bind({ pid: process.pid.toString() });
 
 const job = new cron.CronJob(cronTime, async () => {
   const logger = createLogger(loggerConfig);
