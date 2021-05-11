54998d8af7667714dadda1bb105cba6a0477a798
Samuel Loury <sloury@theblockchainxdev.com>
2021-05-11 11:22:55 +0200

    Remove ad-hoc build script, obsoleted by tilt

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
-docker build -t k3d-registry.localhost:5000/tezos-api-gateway:0.1.0 .
-docker push k3d-registry.localhost:5000/tezos-api-gateway:0.1.0
