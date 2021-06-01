3c778995eaf710baa53d1efe4e024c88fc07e12b
Samuel Loury <sloury@theblockchainxdev.com>
2021-06-01 15:40:15 +0200

    Make the injection worker not be killed by kubernetes
    To do so, we expose some prometheus exporter endpoint.
    
    This endpoint is used by kubernetes in the livenessProbe and the
    readinessProbe to decide whether or not the pod is in a good
    shape.

--- src/processes/workers/injection/index.ts
+++ src/processes/workers/injection/index.ts
@@ -1,8 +1,25 @@
 import { InjectionConsumerProcess } from './injection-consumer-process';
 import { createLogger } from '../../../services/logger';
 import { injectionWorkerLoggerConfig } from './config';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { MeterProvider } from '@opentelemetry/metrics';
 
 if (!module.parent) {
+  // Add your port and startServer to the Prometheus options
+  const options = { port: 9466, startServer: true };
+  const exporter = new PrometheusExporter(options);
+
+  // Register the exporter
+  const meter = new MeterProvider({
+    exporter,
+    interval: 1000,
+  }).getMeter('example-prometheus');
+
+  const counter = meter.createCounter('metric_name', {
+    description: 'Example of a counter',
+  });
+  counter.bind({ pid: process.pid.toString() });
+
   const logger = createLogger(injectionWorkerLoggerConfig);
 
   const injectionConsumerProcess = new InjectionConsumerProcess(logger);
