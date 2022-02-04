56f86b114199ff59e3c8f68e30a0f3c2023082c4
Mathieu Guillotin <mguillotin@theblockchainxdev.com>
2022-02-04 10:18:36 +0100

    Upgrade to 0.8.2

--- src/config/openapi/spec/info/index.ts
+++ src/config/openapi/spec/info/index.ts
@@ -1,7 +1,7 @@
 const info = {
   title: 'tezos-api-gateway',
   description: 'Webservice responsible to interact with Tezos Blockchain',
-  version: '0.8.1',
+  version: '0.8.2',
 };
 
 export default info;
