5b1feffbdeece76efc00832c35b6146178ae10ec
Régis Patroix <rpatroix@eniblock.com>
2023-03-22 16:24:37 +0100

    Upgrade to version 1.0.2

--- src/config/openapi/spec/info/index.ts
+++ src/config/openapi/spec/info/index.ts
@@ -1,7 +1,7 @@
 const info = {
   title: 'tezos-api-gateway',
   description: 'Webservice responsible to interact with Tezos Blockchain',
-  version: '1.0.1',
+  version: '1.0.2',
 };
 
 export default info;
